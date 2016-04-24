68d6cb2ea09676adc0aa743f23ed43a86ae6a5c4
Ali Haghighat doost <alihaghighatdoost@Alis-MBP.lan>
2016-04-24 21:13:05 +0430

    first commit of tapper app

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -10,14 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Properties
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    
+    //Outlets
+    @IBOutlet weak var logoImg : UIImageView!
+    @IBOutlet weak var howManyTapsTxt : UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn : UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    
+    
+    @IBAction func onPlayBtnPressed(sender:UIButton!){
+        
+        if (howManyTapsTxt.text != nil && howManyTapsTxt.text != ""){
+            
+            logoImg.hidden = true
+            howManyTapsTxt.hidden = true
+            playBtn.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            updateTapsLbl()
+        }
+        
     }
+    
+    @IBAction func onTapBtnPressed(sender:UIButton!){
+    
+        currentTaps += 1
+        updateTapsLbl()
+        
+        if isGameOver() {
+            restartGame()
+        }
+    }
+    
+    func restartGame() {
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        logoImg.hidden = false
+        howManyTapsTxt.hidden = false
+        playBtn.hidden = false
+        
+        tapBtn.hidden = true
+        tapsLbl.hidden = true
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func isGameOver() -> Bool {
+        return currentTaps >= maxTaps
+    }
+    
+    func updateTapsLbl() {
+        tapsLbl.text = "\(currentTaps) Taps"
     }
 
 
